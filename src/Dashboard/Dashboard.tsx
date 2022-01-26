e0b823ccf38ea0d72813a9579f5f5b6d5da375ca
Dgiannaris <mtsgiannaris@gmail.com>
2022-01-26 11:25:09 +0200

    add Slider MVP | apply Slider MVP | adjust logic for Slider

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -3,20 +3,46 @@ import { useData } from '../getters/useData';
 import { Navbar } from '../components/Navbar/Navbar';
 import { Tile } from '../components/Tile/Tile';
 import { Application, sortLayers } from '../common/utils';
-import './styles.css';
+import { Slider } from '../components/Slider/Slider';
 import { Spinner } from '../components/Spinner/Spinner';
+import './styles.css';
 
+/**
+ * Main component, that wraps and renders the widgets
+ * @returns 
+ */
 export const Dashboard = () => {
 
-  const { data, loading } = useData(`/data`);
-  const [selected, setSelected] = React.useState([] as Application[]);
+  const { data = [], loading } = useData(`/data`);
+  const [displayed, setDisplayed] = React.useState([] as Application[]);
+  const [max, setMax] = React.useState(0);
+  const [min, setMin] = React.useState(0);
+  const [truth, setTruth] = React.useState([] as Application[]);
+  const [rangeValue, setRangeValue] = React.useState(0);
+
+  React.useEffect(() => {
+    const dataMax = (data && Math.max.apply(Math, data.map((o: Application) => o.spend))) || 1;
+    const dataMin = (data && Math.min.apply(Math, data.map((o: Application) => o.spend))) || 0;
+    setMax(dataMax);
+    setMin(dataMin);
+  },[data]);
+
 
   const onSelect = (data: Application[]) => {
-
     const selectedCategory = [...data]; //immutability
+    const selectedCategoryToSort = [...data]; //immutability
+    const sortedCategory = selectedCategoryToSort.sort((a,b) => (a.spend > b.spend) ? 1 : ((b.spend > a.spend) ? -1 : 0));
+    setMax(sortedCategory[sortedCategory.length -1].spend)
+    setMin(sortedCategory[0].spend)
+    setDisplayed(sortLayers(selectedCategory));
+    setTruth(sortLayers(selectedCategory));
+  }
 
-    setSelected(sortLayers(selectedCategory));
-
+  const onRange = (value: number) => {
+    setRangeValue(value);
+    const rangedSelected = [...truth];
+    const rangedData = rangedSelected.filter((application) => application.spend <= value );
+    setDisplayed(rangedData);
   }
 
   return (
@@ -26,13 +52,18 @@ export const Dashboard = () => {
           <h1>Pharos Dimitris Giannaris</h1>
           <Spinner isLoading={loading} />
         </div>
-        { data && !loading &&
+        { 
+          data && !loading &&
           <Navbar data={data} onSelect={onSelect}  />
+        }
+        { 
+          data &&
+          <Slider min={min} max={max} range={rangeValue} onRange={onRange} />
         }
       </div>
       <div className='tileWrapper'>
         {
-          selected.map((application, idx) => (
+          displayed.map((application, idx) => (
             <Tile key={`${application.id}${idx}`} price={application.spend} title={application.name} />
           ))
         }
---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import './styles.css';
+
+interface Props {
+  min: number;
+  max: number;
+  range: number;
+  onRange: (value: number) => void;
+}
+
+/**
+ * The slider component, decides how many tiles should be rendered according to selected price
+ * @param min The minimum allowed price
+ * @param man The maximum allowed price
+ * @param range The selected range of prices
+ * @param onRange callback function used onChange effect
+ */
+export const Slider = ({min, max, range, onRange}: Props) => {
+  return (
+    <div>
+      <input 
+        className='slider'
+        onChange={(event) => onRange(Number(event.target.value))}
+        type="range"
+        min={`${min}`}
+        max={`${max}`}
+        step="1"
+        value={range || max } 
+      />
+      <div className='rangeWrapper'>
+        <div className='minValue'>{`$${min}`}</div>
+        <div className='maxValue'>{`$${max}`}</div>
+      </div>
+    </div>
+  )
+}
