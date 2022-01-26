eda380999e99f7fb8844da45f37647eb629b3c7e
Dgiannaris <mtsgiannaris@gmail.com>
2022-01-26 11:23:10 +0200

    add Tile jsdoc

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -6,6 +6,12 @@ interface Props {
   price: number;
 }
 
+/**
+ * A basic tile component, used to render an array of applications
+ * @param title The title of the tile/application form
+ * @param price The price of the application
+ * @returns 
+ */
 export const Tile = ({title, price}: Props) => (
   <div className='tile'>
     <h3 className='title'>{title}</h3>
